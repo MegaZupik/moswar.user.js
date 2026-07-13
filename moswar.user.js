a9ce1bc438a863af82286857f9001d00f854d6c4
MegaZupik <82896583+MegaZupik@users.noreply.github.com>
2026-07-13 04:52:06 +0300

    Update moswar.user.js

--- moswar.user.js
+++ moswar.user.js
@@ -2,7 +2,7 @@
 // @name           Moswar крутой
 // @author         Магнус
 // @namespace      Империум человечества
-// @version        8.5
+// @version        8.6
 // @description    лучшатора для мосвара
 // @include        https://*.moswar.ru*
 // @include        https://*.moswar.net*
